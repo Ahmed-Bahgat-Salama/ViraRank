ed1e244daccd27d06baffaae7a152a3b0b7f4ece
ahmed bahgat <119078590+Ahmed-Bahgat-Salama@users.noreply.github.com>
2025-09-12 01:36:50 +0300

    Add files via upload

---
+++ TrendService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+using ViraRankCleanApi.DTOs;
+
+namespace ViraRankCleanApi.Services
+{
+    public class TrendService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TrendService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<TrendAnalysisResponse?> AnalyzeTrend(TrendAnalysisRequest request)
+        {
+         
+            var trendClient = _httpClientFactory.CreateClient("TrendModel");
+
+            
+            var response = await trendClient.PostAsJsonAsync("predict", request);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<TrendAnalysisResponse>();
+        }
+    }
+}
