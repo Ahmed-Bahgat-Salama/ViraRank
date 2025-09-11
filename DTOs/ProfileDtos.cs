2d02c3b7c65eb73976a4677c96a707442d65766f
ahmed bahgat <119078590+Ahmed-Bahgat-Salama@users.noreply.github.com>
2025-09-11 03:25:35 +0300

    Update ProfileDtos.cs

--- DTOs/ProfileDtos.cs
+++ DTOs/ProfileDtos.cs
@@ -1,5 +1,6 @@
-﻿namespace ViraRankCleanApi.DTOs
+namespace ViraRankCleanApi.DTOs
 {
+   
     public class ProfileResponse
     {
         public int Id { get; set; }
@@ -9,4 +10,13 @@
         public bool Gender { get; set; }
         public string? ImageUrl { get; set; }
     }
+    public class UpdateProfileRequest
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? BirthDate { get; set; } 
+        public bool? Gender { get; set; }
+        public string? ImageUrl { get; set; }
+    }
 }
+
