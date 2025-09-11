f08a3ae8eca671377ba9c14561d4a7d1ea9abdc1
ahmed bahgat <119078590+Ahmed-Bahgat-Salama@users.noreply.github.com>
2025-09-12 01:41:06 +0300

    Add files via upload

---
+++ DTOs/TrendDtos.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ViraRankCleanApi.DTOs
+{
+    // كلاس يمثل البيانات التي سنرسلها للموديل
+    public class TrendAnalysisRequest
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = "";
+
+        [JsonPropertyName("platform")]
+        public string Platform { get; set; } = "";
+    }
+
+
+    public class TrendAnalysisResponse
+    {
+        [JsonPropertyName("viral")]
+        public bool IsViral { get; set; }
+
+        [JsonPropertyName("probability")]
+        public float Probability { get; set; }
+    }
+}
